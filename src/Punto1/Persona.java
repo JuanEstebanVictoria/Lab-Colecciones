fa836968aa5f29e002b76a2e544eb30b63ab698a
anaIter <tatianamosqueraiter182@gmail.com>
2024-08-27 19:57:08 -0500

    cambios2

---
+++ src/Punto1/Persona.java
@@ -0,0 +1,40 @@
+package Punto1;
+
+public class Persona implements Comparable<Persona> {
+    private String nombre;
+    private int edad;
+    private String genero;
+
+    public Persona(String nombre, int edad, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+
+    @Override
+    public int compareTo( Persona persona) {
+        return this.nombre.compareTo(persona.getNombre());
+    }
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+
+}
---
+++ src/Punto2/Producto.java
@@ -0,0 +1,39 @@
+package Punto2;
+
+public class Producto implements Comparable<Producto> {
+    private String codigo;
+    private String nombre;
+    private double precio;
+
+    public Producto(String codigo, String nombre, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    @Override
+    public int compareTo(Producto otroProducto) {
+        return this.codigo.compareTo(otroProducto.getCodigo());
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}

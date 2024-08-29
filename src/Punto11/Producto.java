fb84877132de865588d2644dfffbfc0f5afe2649
JuanEstebanVictoria <juane.victorial@uqvirtual.edu.co>
2024-08-29 18:41:33 -0500

    Create Producto.java

---
+++ src/Punto11/Producto.java
@@ -0,0 +1,13 @@
+package Punto11;
+
+public class Producto {
+    private double producto;
+
+    public Producto(double producto) {
+        this.producto = producto;
+    }
+
+    public double getProducto() {
+        return producto;
+    }
+}

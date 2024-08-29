fe51c008d5279b1b2ed26ac2bca813aaee1d002d
JuanEstebanVictoria <juane.victorial@uqvirtual.edu.co>
2024-08-29 18:41:26 -0500

    11

---
+++ src/Punto11/Quicksort.java
@@ -0,0 +1,33 @@
+package Punto11;
+
+import Punto7.Persona;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quicksort {
+
+    public static void main(String[] args) {
+
+
+        List<Producto>productos = new ArrayList<>();
+
+        productos.add(new Producto(1));
+        productos.add(new Producto(14));
+        productos.add(new Producto(31));
+        productos.add(new Producto(5));
+        productos.add(new Producto(19));
+    }
+
+    public static List<Producto> ordenarQuicksort (List<Producto>productos){
+
+        if(productos.isEmpty()){
+            return  new ArrayList<>();
+        }
+
+
+
+
+
+    }
+}
